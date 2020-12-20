cc822e7cbe5c5311616398b7262efde0834a4080
CetinKaltar <cetin.kaltar@bankish.eu>
2020-12-20 02:30:35 +0200

    styled components added to our project and transform some css to styled jsx components for optional styles

--- src/pages/hompage/homepage.component.jsx
+++ src/pages/hompage/homepage.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './homepage.styles.scss';
+import {HomePageContainer} from './homepage-styles';
 import Directory from '../../components/directory/directory.component';
 
-const Homepage = ({ history }) => (
-  <div className="homepage">
+const Homepage = () => (
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default Homepage;
